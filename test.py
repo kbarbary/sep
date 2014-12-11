3a5f3d5c39892bf68310a0b0f41c0c380e6183b5
Kyle Barbary <kylebarbary@gmail.com>
2014-12-11 13:13:55 -0800

    organize tests

--- test.py
+++ test.py
@@ -31,13 +31,13 @@ IMAGECAT_DTYPE = [('number', np.int64),
                   ('flux_auto', np.float64),
                   ('fluxerr_auto', np.float64),
                   ('flags', np.int64)]
-IMAGE_DTYPES = [np.float64, np.float32, np.int32]  # supported image dtypes
+SUPPORTED_IMAGE_DTYPES = [np.float64, np.float32, np.int32]
 
 def assert_allclose_structured(x, y):
     """Assert that two structured arrays are close.
 
-    Compares floats relatively and everything else exactly."""
-
+    Compares floats relatively and everything else exactly.
+    """
     assert x.dtype == y.dtype
     for name in x.dtype.names:
         if np.issubdtype(x.dtype[name], float):
@@ -45,17 +45,22 @@ def assert_allclose_structured(x, y):
         else:
             assert_equal(x[name], y[name])
 
+# If we have a FITS reader, read in the necessary test images
+if not NO_FITS:
+    image_data = getdata(IMAGE_FNAME)
+    image_refback = getdata(BACKIMAGE_FNAME)
+
+# -----------------------------------------------------------------------------
+# Test versus Source Extractor results
 
 @pytest.mark.skipif(NO_FITS, reason="no FITS reader") 
 def test_vs_sextractor():
-    data = getdata(IMAGE_FNAME)
-
+    data = np.copy(image_data)  # make an explicit copy so we can 'subfrom'
     bkg = sep.Background(data, bw=64, bh=64, fw=3, fh=3)
 
     # Test that SExtractor background is same as SEP:
     bkgarr = bkg.back(dtype=np.float32)
-    refbackarr = getdata(BACKIMAGE_FNAME)
-    assert_allclose(bkgarr, refbackarr, rtol=1.e-5)
+    assert_allclose(bkgarr, image_refback, rtol=1.e-5)
 
     # Extract objects
     bkg.subfrom(data)
@@ -76,9 +81,9 @@ def test_vs_sextractor():
     assert_allclose(flux, refobjs['flux_aper'], rtol=2.e-4)
     assert_allclose(fluxerr, refobjs['fluxerr_aper'], rtol=1.0e-5)
 
-    # check if the flag functions work at all
-    assert sep.istruncated(flag).sum() == 4
-    assert sep.hasmasked(flag).sum() == 0
+    # check if the flags work at all (comparison values 
+    assert ((flag & sep.APER_TRUNC) != 0).sum() == 4
+    assert ((flag & sep.APER_HASMASKED) != 0).sum() == 0
 
     # Test "flux_auto"
     kr, flag = sep.kron_radius(data, objs['x'], objs['y'], objs['a'],
@@ -118,100 +123,9 @@ def test_vs_sextractor():
     assert_allclose(cyy, objs['cyy'], rtol=1.e-4)
     assert_allclose(cxy, objs['cxy'], rtol=1.e-4)
 
-def test_extract_noise_array():
-
-    # Get some background-subtracted test data:
-    data = getdata(IMAGE_FNAME)
-    bkg = sep.Background(data, bw=64, bh=64, fw=3, fh=3)
-    bkg.subfrom(data)
-
-    # Ensure that extraction with constant noise array gives the expected
-    # result. We have to use conv=None here because the results are *not*
-    # the same when convolution is on! This is because the noise map is
-    # convolved. Near edges, the convolution doesn't adjust for pixels
-    # off edge boundaries. As a result, the convolved noise map is not
-    # all ones.
-    objects = sep.extract(data, 1.5*bkg.globalrms, conv=None)
-    objects2 = sep.extract(data, 1.5*bkg.globalrms, err=np.ones_like(data),
-                           conv=None)
-    assert_equal(objects, objects2)  # we can test exact match here.
-
-    # Less trivial test where thresh is realistic. Still a flat noise map.
-    noise = bkg.globalrms * np.ones_like(data)
-    objects2 = sep.extract(data, 1.5, err=noise, conv=None)
-    assert_equal(objects, objects2)
-
-
-def test_byte_order_exception():
-    """Test that error about byte order is raised with non-native
-    byte order input array."""
-
-    data = np.ones((100, 100), dtype=np.float64)
-    data = data.byteswap(True).newbyteorder()
-    with pytest.raises(ValueError) as excinfo:
-        bkg = sep.Background(data)
-    assert 'byte order' in str(excinfo.value)
-
 
 # -----------------------------------------------------------------------------
-# aperture tests
-
-def test_aperture_dtypes():
-    naper = 100
-    x = np.random.uniform(200., 800., naper)
-    y = np.random.uniform(200., 800., naper)
-    r = 3.
-    fluxes = []
-    for dt in IMAGE_DTYPES:
-        data = np.ones((1000, 1000), dtype=dt)
-        flux, fluxerr, flag = sep.sum_circle(data, x, y, r)
-        fluxes.append(flux)
-
-    for i in range(1, len(fluxes)):
-        assert_allclose(fluxes[0], fluxes[i])
-
-
-def test_apertures_exact():
-    naper = 100
-    x = np.random.uniform(200., 800., naper)
-    y = np.random.uniform(200., 800., naper)
-    theta = np.random.uniform(-np.pi/2., np.pi/2.)
-    ratio = np.random.uniform(0.2, 1.0, naper)
-    r = 3.
-    for dt in IMAGE_DTYPES:
-        data = np.ones((1000, 1000), dtype=dt)
-        for r in [0.5, 1., 3.]:
-            flux, fluxerr, flag = sep.sum_circle(data, x, y, r, subpix=0)
-            assert_allclose(flux, np.pi*r**2)
-
-            rout = r*1.1
-            flux, fluxerr, flag = sep.sum_circann(data, x, y, r, rout,
-                                                  subpix=0)
-            assert_allclose(flux, np.pi*(rout**2 - r**2))
-
-            flux, fluxerr, flag = sep.sum_ellipse(data, x, y, 1., ratio,
-                                                  theta, r=r, subpix=0)
-            assert_allclose(flux, np.pi*ratio*r**2)
-
-            rout = r*1.1
-            flux, fluxerr, flag = sep.sum_ellipann(data, x, y, 1., ratio,
-                                                   theta, r, rout, subpix=0)
-            assert_allclose(flux, np.pi*ratio*(rout**2 - r**2))
-
-
-# -----------------------------------------------------------------------------
-
-def test_mask_ellipse():
-    arr = np.zeros((20, 20), dtype=np.bool)
-
-    # should mask 5 pixels:
-    sep.mask_ellipse(arr, 10., 10., 1.0, 1.0, 0.0, r=1.001)
-    assert arr.sum() == 5
-
-    # should mask 13 pixels:
-    sep.mask_ellipse(arr, 10., 10., 1.0, 1.0, 0.0, r=2.001)
-    assert arr.sum() == 13
-
+# Background
 
 def test_masked_background():
     data = 0.1 * np.ones((6,6))
@@ -241,3 +155,149 @@ def test_masked_background():
     sky = sep.Background(data, mask=mask, bw=3, bh=3, fw=1, fh=1)
     assert_approx_equal(sky.globalback, 0.1)
     assert_allclose(sky.back(), 0.1 * np.ones((6, 6)))
+
+
+# -----------------------------------------------------------------------------
+# Extract
+
+@pytest.mark.skipif(NO_FITS, reason="no FITS reader") 
+def test_extract_with_noise_array():
+
+    # Get some background-subtracted test data:
+    data = np.copy(image_data)
+    bkg = sep.Background(data, bw=64, bh=64, fw=3, fh=3)
+    bkg.subfrom(data)
+
+    # Ensure that extraction with constant noise array gives the expected
+    # result. We have to use conv=None here because the results are *not*
+    # the same when convolution is on! This is because the noise map is
+    # convolved. Near edges, the convolution doesn't adjust for pixels
+    # off edge boundaries. As a result, the convolved noise map is not
+    # all ones.
+    objects = sep.extract(data, 1.5*bkg.globalrms, conv=None)
+    objects2 = sep.extract(data, 1.5*bkg.globalrms, err=np.ones_like(data),
+                           conv=None)
+    assert_equal(objects, objects2)
+
+    # Less trivial test where thresh is realistic. Still a flat noise map.
+    noise = bkg.globalrms * np.ones_like(data)
+    objects2 = sep.extract(data, 1.5, err=noise, conv=None)
+    assert_equal(objects, objects2)
+
+
+# -----------------------------------------------------------------------------
+# aperture tests
+
+def test_aperture_dtypes():
+    """Ensure that all supported image dtypes work in sum_circle() and
+    give the same answer"""
+    naper = 100
+    x = np.random.uniform(200., 800., naper)
+    y = np.random.uniform(200., 800., naper)
+    r = 3.
+    fluxes = []
+    for dt in SUPPORTED_IMAGE_DTYPES:
+        data = np.ones((1000, 1000), dtype=dt)
+        flux, fluxerr, flag = sep.sum_circle(data, x, y, r)
+        fluxes.append(flux)
+
+    for i in range(1, len(fluxes)):
+        assert_allclose(fluxes[0], fluxes[i])
+
+
+def test_apertures_all():
+    """Test that aperture subpixel sampling works"""
+
+    naper = 100
+    np.random.seed(5)
+    data = np.random.rand(1000, 1000)
+    x = np.random.uniform(200., 800., naper)
+    y = np.random.uniform(200., 800., naper)
+    r = 3.
+    rtol=1.e-8
+
+    for subpix in [0, 1, 5]:
+        flux_ref, fluxerr_ref, flag_ref = sep.sum_circle(data, x, y, r,
+                                                         subpix=subpix)
+
+        flux, fluxerr, flag = sep.sum_circann(data, x, y, 0., r,
+                                              subpix=subpix)
+        assert_allclose(flux, flux_ref, rtol=rtol)
+
+        # TODO: remove this constraint once bug in elliptical-exact is fixed.
+        if subpix == 0:
+            continue
+
+        flux, fluxerr, flag = sep.sum_ellipse(data, x, y, r, r, 0.,
+                                              subpix=subpix)
+        assert_allclose(flux, flux_ref, rtol=rtol)
+
+        flux, fluxerr, flag = sep.sum_ellipse(data, x, y, 1., 1., 0., r=r,
+                                              subpix=subpix)
+        assert_allclose(flux, flux_ref, rtol=rtol)
+
+
+
+def test_apertures_exact():
+    """Test area as measured by exact aperture modes on array of ones"""
+    naper = 100
+    x = np.random.uniform(200., 800., naper)
+    y = np.random.uniform(200., 800., naper)
+    theta = np.random.uniform(-np.pi/2., np.pi/2.)
+    ratio = np.random.uniform(0.2, 1.0, naper)
+    r = 3.
+    for dt in SUPPORTED_IMAGE_DTYPES:
+        data = np.ones((1000, 1000), dtype=dt)
+        for r in [0.5, 1., 3.]:
+            flux, fluxerr, flag = sep.sum_circle(data, x, y, r, subpix=0)
+            assert_allclose(flux, np.pi*r**2)
+
+            rout = r*1.1
+            flux, fluxerr, flag = sep.sum_circann(data, x, y, r, rout,
+                                                  subpix=0)
+            assert_allclose(flux, np.pi*(rout**2 - r**2))
+
+            flux, fluxerr, flag = sep.sum_ellipse(data, x, y, 1., ratio,
+                                                  theta, r=r, subpix=0)
+            assert_allclose(flux, np.pi*ratio*r**2)
+
+            rout = r*1.1
+            flux, fluxerr, flag = sep.sum_ellipann(data, x, y, 1., ratio,
+                                                   theta, r, rout, subpix=0)
+            assert_allclose(flux, np.pi*ratio*(rout**2 - r**2))
+
+
+def test_mask_ellipse():
+    arr = np.zeros((20, 20), dtype=np.bool)
+
+    # should mask 5 pixels:
+    sep.mask_ellipse(arr, 10., 10., 1.0, 1.0, 0.0, r=1.001)
+    assert arr.sum() == 5
+
+    # should mask 13 pixels:
+    sep.mask_ellipse(arr, 10., 10., 1.0, 1.0, 0.0, r=2.001)
+    assert arr.sum() == 13
+
+
+# -----------------------------------------------------------------------------
+# General behavior and utilities
+
+def test_byte_order_exception():
+    """Test that error about byte order is raised with non-native
+    byte order input array. This should happen for Background, extract,
+    and aperture functions."""
+
+    data = np.ones((100, 100), dtype=np.float64)
+    data = data.byteswap(True).newbyteorder()
+    with pytest.raises(ValueError) as excinfo:
+        bkg = sep.Background(data)
+    assert 'byte order' in str(excinfo.value)
+
+
+def test_set_pixstack():
+    """Ensure that setting the pixel stack size works."""
+    old = sep.get_extract_pixstack()
+    new = old * 2
+    sep.set_extract_pixstack(new)
+    assert new == sep.get_extract_pixstack()
+    sep.set_extract_pixstack(old)
